bd3533d71cdbe6f79ced420b513e9eeb3b93ec2f
root <mohammad.furqan@10xengineers.ai>
2022-05-31 20:42:05 +0800

    Cleaned Up Code

--- Project/.synopsys_dve_rebuild.tcl
+++
@@ -1,9 +0,0 @@
-# DVE Simulation Rebuild/Restart Options
-# Saved on Thu May 12 17:40:25 2022
-set SIMSETUP::REBUILDOPTION 1
-set SIMSETUP::REBUILDCMD {rm -rf csrc simv.daidir simv tr_db.log ucli.key vc_hdrs.h  vcs -ntb_opts uvm-1.2 -sverilog -f flist -LDFLAGS -full64 -debug_all +lint=TFIPC-L}
-set SIMSETUP::REBUILDDIR {}
-set SIMSETUP::RESTOREBP 1
-set SIMSETUP::RESTOREDUMP 1
-set SIMSETUP::RESTOREFORCE 1
-set SIMSETUP::RESTORESPECMAN 0
--- Project/csrc/filelist.cu
+++ Project/csrc/filelist.cu
@@ -1,12 +1,12 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _16308_archive_1.so
-_16308_archive_1.so : archive.0/_16308_archive_1.a
+ARCHIVE_OBJS += _35222_archive_1.so
+_35222_archive_1.so : archive.0/_35222_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -o .//../simv.daidir//_16308_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -o .//../simv.daidir//_35222_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_16308_archive_1.so $@
+	@ln -sf .//../simv.daidir//_35222_archive_1.so $@
 
 
 
